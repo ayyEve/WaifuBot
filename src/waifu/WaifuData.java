4280e34c7ba302774b822456fd41174319e9591f
Eve <hi.its.eve@gmail.com>
2019-05-15 08:29:18 -0400

    Update WaifuData.java
    updated formatting

--- src/waifu/WaifuData.java
+++ src/waifu/WaifuData.java
@@ -1,25 +1,24 @@
 package waifu;
 
-public class WaifuData 
-{
-	public String names;
-	int count;
+public class WaifuData {
+  public String names;
+  int count;
 	
-	public WaifuData()
-	{
-		names = "";
-		count = 0;
-	}
+  public WaifuData() {
+    names = "";
+    count = 0;
+  }
 	
-	public WaifuData(String names, int count)
-	{
-		this.names = names;
-		this.count = count;
-	}
+  public WaifuData(String names, int count) {
+    this.names = names;
+    this.count = count;
+  }
 	
-	public String get()
-	{
-		if (count == 0) return "no " + Main.getNameText() + "s";
-		return count + " " + Main.getNameText() + (count > 1 ? "s" : "") + ":\n" + names;
-	}
+  public String get() {
+    if (count == 0) {
+      return "no " + Main.getNameText() + "s";
+    }
+
+    return count + " " + Main.getNameText() + (count > 1 ? "s" : "") + ":\n" + names;
+  }
 }
